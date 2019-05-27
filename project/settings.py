4e8977cb75898273957d9c75d87d91c32fa4d56a
SergeyV1993 <sergey.vlasov3333@gmail.com>
2019-05-27 19:54:39 +0300

    disable cache

--- project/settings.py
+++ project/settings.py
@@ -100,7 +100,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        #'BACKEND': 'django.core.cache.backends.locmem.LocMemCache', №сохраняет кэш в оперативке
+         'BACKEND': 'django.core.cache.backends.dummy.DummyCache', #не сохраняет кэш, нужен для отладки
     }
 }
 
