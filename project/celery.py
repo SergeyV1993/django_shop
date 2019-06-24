b40d80be65cd597676c3f7debb79f2babcfab3d0
SergeyV1993 <sergey.vlasov3333@gmail.com>
2019-06-24 19:11:09 +0300

    change celery files

--- project/celery.py
+++ project/celery.py
@@ -7,5 +7,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 app = Celery('project')
 app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.autodiscover_tasks()
 
