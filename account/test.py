7350ecfdbb2e322cb3266f381e0b0dc30ea5639a
Vlasov Sergei <vlasov@vlasov-macbookair-2.local>
2019-11-13 18:50:18 +0300

    add test for account

--- account/test.py
+++ account/test.py
@@ -1,11 +1,57 @@
+import datetime
 from django.test import TestCase
 from django.shortcuts import *
+from django.contrib.auth.models import User
+from orders.models import *
 
 
 class AccountListViewTest(TestCase):
 
-    def test_view_url_accessible(self):
-        resp = self.client.get(reverse('account'))
-        self.assertEqual(resp.status_code, 200)
+    def setUp(self):
+        # Создание тестового пользователя
+        test_user1 = User.objects.create_user(username='testuser', password='12345')
+        test_user1.save()
+
+        number_of_orders_copies = 10
+        for order in range(number_of_orders_copies):
+            Order.objects.create(user=test_user1, customer_email='test@test.qw', customer_name='test',
+                                 customer_phone='1234567', total_price='1000', create=datetime.datetime.now(),
+                                 update=datetime.datetime.now())
+
+    def test_logged_user(self):
+        self.client.login(username='testuser', password='12345')
+        response = self.client.get(reverse('account'))
+
+        # Проверка что пользователь залогинился
+        self.assertEqual(str(response.context['user']), 'testuser')
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('account'))
+        self.assertEqual(response.status_code, 200)
+
+        self.assertTemplateUsed(response, 'account/account.html')
+
+    def test_count_orders_for_user(self):
+        self.client.login(username='testuser', password='12345')
+        response = self.client.get(reverse('account'))
+
+        get_user = User.objects.get(username=str(response.context['user']))
+        get_orders = Order.objects.filter(user=get_user)
+
+        self.assertEqual(len(get_orders), 10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
