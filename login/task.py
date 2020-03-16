8b523a57e054a5665f7cfe7d4c33e68f8edb863a
Vlasov Sergei <vlasov@vlasov-macbookair-2.local>
2020-03-16 19:26:34 +0300

    add retry for login celery task

--- login/task.py
+++ login/task.py
@@ -2,7 +2,7 @@ from project.celery import app
 from django.core.mail import send_mail
 
 
-@app.task
+@app.task(default_retry_delay=2 * 60)
 def send_mail_login(username):
     subject = 'Авторизация на сайте'
     message = 'Выполнен вход под именем {}'.format(username)
