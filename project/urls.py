0ce6eda8e992dba0c45efd551740cb3074021e62
Vlasov Sergei <vlasov@vlasov-macbookair-2.local>
2020-05-01 10:16:19 +0300

    fix cs

--- project/urls.py
+++ project/urls.py
@@ -1,18 +1,3 @@
-"""project URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import *
 from django.conf.urls.static import static
@@ -32,43 +17,58 @@ urlpatterns = [
     path('', include('discount.urls')),
     path('', include('search.urls')),
 
-    re_path(r'^logout/$', LogoutView.as_view(next_page='shop'), name='logout'),
+    path(
+        'logout/',
+        LogoutView.as_view(next_page='shop'),
+        name='logout'
+    ),
 
-    re_path(r'^change_password/$',
-            PasswordChangeView.as_view(
-                template_name='password/change_password.html',
-                success_url=reverse_lazy('change_password_done')), name='change_password'
+    path(
+        'change_password/',
+        PasswordChangeView.as_view(
+            template_name='password/change_password.html',
+            success_url=reverse_lazy('change_password_done')
+        ),
+        name='change_password'
+    ),
+
+    path(
+        'change_password_done/',
+        PasswordChangeDoneView.as_view(template_name='password/change_password_done.html'),
+        name='change_password_done'
+    ),
+
+    path(
+        'reset_password/',
+        PasswordResetView.as_view(
+            template_name='password/reset_password.html',
+            subject_template_name='password/reset_subject.txt',
+            email_template_name='password/reset_body.html',
+            success_url=reverse_lazy('reset_password_done')
+        ),
+        name='reset_password'
+    ),
+
+    path(
+        'reset_password_done/',
+        PasswordResetDoneView.as_view(template_name='password/reset_password_done.html'),
+        name='reset_password_done'
+    ),
+
+    re_path(
+        r'^reset_password_confirmation/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        PasswordResetConfirmView.as_view(
+            template_name='password/reset_password_confirmation.html',
+            success_url=reverse_lazy('reset_password_confirm_done')
             ),
+        name='reset_password_confirm'
+    ),
 
-    re_path(r'^change_password_done/$',
-            PasswordChangeDoneView.as_view(
-                template_name='password/change_password_done.html'), name='change_password_done'
-            ),
-
-
-    re_path(r'^reset_password/$',
-            PasswordResetView.as_view(
-                template_name='password/reset_password.html',
-                subject_template_name='password/reset_subject.txt',
-                email_template_name='password/reset_body.html',
-                success_url=reverse_lazy('reset_password_done')), name='reset_password'
-            ),
-
-    re_path(r'^reset_password_done/$',
-            PasswordResetDoneView.as_view(
-                template_name='password/reset_password_done.html'), name='reset_password_done'
-            ),
-
-    re_path(r'^reset_password_confirmation/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-            PasswordResetConfirmView.as_view(
-                template_name='password/reset_password_confirmation.html',
-                success_url=reverse_lazy('reset_password_confirm_done')), name='reset_password_confirm'
-            ),
-
-    re_path(r'^reset_password_confirmation_done/$',
-            PasswordResetCompleteView.as_view(
-                template_name='password/reset_password_confirmation_done.html'), name='reset_password_confirm_done'
-            ),
+    path(
+        'reset_password_confirmation_done/',
+        PasswordResetCompleteView.as_view(template_name='password/reset_password_confirmation_done.html'),
+        name='reset_password_confirm_done'
+    ),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
