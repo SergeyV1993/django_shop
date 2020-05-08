06aace65986ed64e11bdf3d2c75c01553935663a
Vlasov Sergei <vlasov@vlasov-macbookair-2.local>
2020-05-08 19:20:26 +0300

    add middleware info

--- categories/middleware.py
+++ categories/middleware.py
@@ -3,7 +3,7 @@ from categories.models import Category
 
 
 class GetCategory(MiddlewareMixin):
-
+    """Middleware для категорий в шапке base.html"""
     def process_request(self, request):
         category_list = Category.objects.only('name').all()
         request.category_list = category_list
