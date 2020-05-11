3b39e3e724b6795dddf258567c8347b34ea4fb6c
Vlasov Sergei <vlasov@vlasov-macbookair-2.local>
2020-05-11 09:29:15 +0300

    fix api for shop

--- shop/api.py
+++ shop/api.py
@@ -1,9 +1,8 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ReadOnlyModelViewSet
 from products.models import *
 from shop.serializers import ShopViewSerializer
 
 
-class ShopViewSet(ModelViewSet):
+class ShopViewSet(ReadOnlyModelViewSet):
     queryset = Product.objects.only('id', 'name', 'price').filter(is_active=True)
     serializer_class = ShopViewSerializer
-
--- shop/urls.py
+++ shop/urls.py
@@ -1,31 +1,16 @@
-"""project URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import path
 from shop.views import *
 from rest_framework.routers import DefaultRouter
 from shop.api import ShopViewSet
 
-router = DefaultRouter()
-router.register('api/shop_view', ShopViewSet)
-
 urlpatterns = [
     path('shop/', ShopView.as_view(), name='shop'),
     path('shop_vue/', TemplateView.as_view(template_name='shop/shop_vue.html')),
-    # для функции
+    # For function
     # path('shop/', views.shop, name='shop'),
 ]
 
+"""For API"""
+router = DefaultRouter()
+router.register('api/shop_view', ShopViewSet, basename='shop')
 urlpatterns += router.urls
